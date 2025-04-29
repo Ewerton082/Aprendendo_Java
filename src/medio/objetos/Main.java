7689eaef2ccd66efd400cf25ab586e819c8f6f7c
Ewerton Douglas <Tec.-.Ewerton@hotmail.com>
2025-04-29 07:41:31 -0300

    começando sobre classes

--- src/medio/objetos/Main.java
+++ src/medio/objetos/Main.java
@@ -4,14 +4,24 @@ public class Main {
     public static void main(String[] args) {
         // Agora o pau vai torar
 
+        
+        // Criar naruto usando o molde da NOSSA CLASSE Ninjas
         Ninja Naruto = new Ninja();
         Naruto.nome = "Naruto Uzumaki";
         Naruto.idade = 17;
         Naruto.aldeia = "Konoha";
 
+        // Criando o Sasuke, Lembrando que ele é sempre um objeto
         Ninja Sasuke = new Ninja();
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.idade = 15;
         Sasuke.aldeia = "renegado";
+        
+        // Criando a Sakura - Tudo é um OBJETO
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.idade = 16;
+        Sakura.aldeia = "Konoha";
+
     }
 }
