abeb99fd0bd66df38039ccfa135a1137fd0a0a97
Ewerton082 <Tec.-.Ewerton@hotmail.com>
2025-06-09 18:38:12 -0300

    ADD - Adicionando Novo assunto a ser estudado "Encapsulamento"

---
+++ src/medio/encapsulamento/Ninja.java
@@ -0,0 +1,13 @@
+package medio.encapsulamento;
+
+public class Ninja {
+
+    // private é o termo que faz com que os atributos ou metodos n sejam alterados ou acessados por outras classes
+
+    private String nome;
+    private String aldeia;
+    private int idade;
+
+    // para acessar elas voce precisa usar por metods que por padrão costuma-se usar 2 keywords no metodos
+    // GET_nome:pegar o valor | SET_nome: para alterar o nome
+}
